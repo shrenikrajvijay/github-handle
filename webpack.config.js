0e24a3d4aaaf33aa5a46c5b776d3f9bf87a3a77a
Vijay Shrenikraj <s.vijay0991@gmail.com>
2017-12-13 15:30:37 -0800

    did webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var debug = process.env.NODE_ENV !== "production"
+var glob = require('glob')
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+  context: __dirname,
+  devtool: debug ? "inline-sourcemap" : null,
+  entry: glob.sync("./app/scripts/!(*.min.js)/*.js"),
+  output: {
+    path: __dirname + "/app/scripts",
+    filename: "bundle.min.js"
+  },
+    module: {
+        loaders: [
+            { test: /\.css$/, loader: "style-loader!css-loader" },
+            { test: /\.scss$/, loader: 'style-loader!css-loader!sass-loader' }
+        ]
+  },
+  plugins: debug ? [new ExtractTextPlugin('style.css')] : [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false })
+  ]
+};
