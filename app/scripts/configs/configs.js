e5e451525797a1b8d6e69abeff05d8a577566db5
Vijay Shrenikraj <s.vijay0991@gmail.com>
2017-12-13 17:07:35 -0800

    added angualar bootstrap library

--- app/scripts/configs/configs.js
+++ app/scripts/configs/configs.js
@@ -1,8 +1,9 @@
 require('../../styles/main.scss');
 require('../../../bower_components/angular/angular.js');
 require('../../../bower_components/angular-mocks/angular-mocks.js');
+require('../../../bower_components/angular-bootstrap/ui-bootstrap.min.jsx');
 
-angular.module("github", [])
+angular.module("github", ['ui.bootstrap'])
     .constant('API_GITHUB', 'https://api.github.com')
     .constant('GET_USER', 'users')
     .constant('SEARCH_USERS', 'search/users');
